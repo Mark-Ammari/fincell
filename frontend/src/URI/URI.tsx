6b6678696a2e26b72e6dc0db2f61329dfcc55be0
Mark Ammari <mark.ammari@gmail.com>
2021-06-06 00:55:45 -0400

    Fixed URI

--- frontend/src/URI/URI.tsx
+++ frontend/src/URI/URI.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URI = "http://localhost:3000/api/v1/company-data";
+const URI = process.env.NODE_ENV === "production" ? "https://fincell.herokuapp.com/api/v1/company-data" : "http://localhost:3000/api/v1/company-data";
 
 const baseURI = axios.create({
     baseURL: URI
